f0f12a0cbba819da0e901962e526ae6ae593e860
DK <dkdan94@gmail.com>
2018-10-04 12:10:16 -0400

    flex nav items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,16 @@ class Nav extends Component {
   render() {
     return (
       <div className="navbar">
-        <img src="http://youngtron.com/wp-content/uploads/2015/06/youngtronlogo1.png" />
+        <img
+          src="http://youngtron.com/wp-content/uploads/2015/06/youngtronlogo1.png"
+          className="logo"
+        />
+        <div className="nav-items">
+          <div>About</div>
+          <div>Services</div>
+          <div>Resources</div>
+          <div>Location</div>
+        </div>
       </div>
     );
   }
